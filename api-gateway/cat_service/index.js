2285c63e0a756189597a5bb9fba5fcc05e46d2b2
Cuong Duy Nguyen <cuongw.me@gmail.com>
2020-03-02 00:07:04 +0700

    API Gateway (#5)

---
+++ api-gateway/cat_service/index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => 
+    res.json('xin chào from cat_service')
+);
+
+app.listen(3300, () => 
+    console.log('cat_service is listening on port 3300.')
+);
--- reverse_proxy_basic/cat_service/index.js
+++ reverse_proxy_basic/cat_service/index.js
@@ -2,6 +2,10 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (req, res) => res.send('xin chào'));
+app.get('/', (req, res) =>
+    res.send('xin chào')
+);
 
-app.listen(3000, () => console.log('cat_service is listening on port 3000.'));
+app.listen(3000, () =>
+    console.log('cat_service is listening on port 3000.')
+);
